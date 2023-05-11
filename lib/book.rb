e047b86f9cfd455abcce300fd109e5d4729f4305
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-05-10 17:37:49 -0700

    commit tree a0dd2f01d692c525f30bfa76b8bae396cca05ebf

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+class Book
+  attr_accessor :title, :author, :page_count, :genre
 
+  def initialize(title)
+    @title = title
+  end
 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
+
+moby_dick = Book.new("Moby Dick")
+puts moby_dick.title
+puts moby_dick.turn_page
